e4710df1dc54fdfc1c147701880223ec46423dd0
Dominic May <jack.thatch@gmail.com>
2013-01-27 22:47:13 +0800

    Minimum site

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Copyright 2012 Galaxy Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(os.environ.get('PORT', 8888))
+    tornado.ioloop.IOLoop.instance().start()
